adace5ece8927a9c34a75466d285f7ab8006fbae
alistairdevine <alistair.devine98@gmail.com>
2021-09-16 23:44:22 +0100

    Album integration tests implementation
    Unable to run on PC, error note message  - Waiting to load.

--- CodeTest/ApiTesting/IntegrationTests/AlbumIntegration.cs
+++ CodeTest/ApiTesting/IntegrationTests/AlbumIntegration.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
 
 namespace ApiTesting
 {
-    public class AlbumIntegration
+    [Parallelizable(ParallelScope.Self)]
+    public class AlbumIntegration : PlaywrightTest
     {
+        [Test]
+        public async Task VerifyAlbumsRoute()
+        {
+            await using var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true
+            });
+            var context = await browser.NewContextAsync();
 
+            //Open new page
+            var page = await context.NewPageAsync();
+
+            //Go to https://localhost:44344/album
+            await page.GotoAsync("https://localhost:44344/album");
+
+            Assert.AreEqual("https://localhost:44344/album", page.Url);
+        }
+
+        [Test]
+        public async Task VerifyPhotosRoute()
+        {
+            await using var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true
+            });
+            var context = await browser.NewContextAsync();
+
+            //Open new page
+            var page = await context.NewPageAsync();
+
+            //Go to https://localhost:44344/album/1
+            await page.GotoAsync("https://localhost:44344/album/1");
+
+
+
+            Assert.AreEqual("https://localhost:44344/album/1", page.Url);
+        }
     }
 }
