ac43e2820280d4a25e1889de727824403442bc21
alistairdevine <alistair.devine98@gmail.com>
2021-09-17 01:00:12 +0100

    Benchmarking album controller set up.

---
+++ CodeTest/ApiBenchmark/AlbumBenchmark/AlbumBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using CodeTest.Controllers;
+using CodeTest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiBenchmark.AlbumBenchmark
+{
+    [MemoryDiagnoser]
+    public class AlbumBenchmark
+    {
+        public const int id = 1;
+
+        //RESEARCH : Implement an interface for the controller???
+        private static readonly AlbumController _album = new AlbumController();
+
+        [Benchmark(Baseline = true)]
+        public void GetAlbums()
+        {
+            _album.GetAlbumsController();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void GetAlbumById()
+        {
+            _album.GetAlbumById(id);
+        }
+    }
+}
